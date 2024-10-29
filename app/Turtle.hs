9e7f7c6ca33d33dc28294096e8b346eb6c0711b0
Herbert Lange <github@pietaetskirsche.de>
2024-10-29 16:43:25 +0100

    work on running

--- app/Turtle.hs
+++ app/Turtle.hs
@@ -1,6 +1,10 @@
 module Turtle (Operation(..), home) where
 
+import Control.Monad.Trans.State.Lazy
+import Data.List
+
 data Operation = Forward | Backward | Left | Right | Pen | Heading | Position | Push Int | Pop | Dup | Swap | Add | Sub | Label Int | Jumpz Int deriving Show
+
 
 home :: [Operation]
 home =
@@ -12,3 +16,13 @@ home =
   , Sub
   , Turtle.Left
   ]
+type Canvas = [[Int]]
+
+run :: [Operation] -> Int -> Int -> [Canvas]
+run ops =
+  let (result, state) = runState (evalOps ops) []
+  in
+    nub result
+  where
+    evalOps :: [Operation] -> State [Int] [Canvas]
+    evalOps [] = return []
